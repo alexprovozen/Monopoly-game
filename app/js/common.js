b15e22d7e7688dc07c2f0671666cb8ba3e1b4788
Alexander <alxfedus@gmail.com>
2018-05-25 20:52:40 +0300

    Last update 25 may

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,12 @@
 $(function() {
 
+var cellWidth = $('.cell').width();
+$('.cell-horizontal').css('height', cellWidth);
+$('.cell-horizontal-big').css('height', cellWidth*1.5);
 
-
+$(window).on('resize', function() {
+	cellWidth = $('.cell').width();
+	$('.cell-horizontal').css('height', cellWidth);
+	$('.cell-horizontal-big').css('height', cellWidth*1.5);
+});
 });
